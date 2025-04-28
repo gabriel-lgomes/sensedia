6c5f150fe7de8a7539b5b0d9ea46c45d6f3b89f5
gabriel-lgomes <gabriel_lopes.g@hotmail.com>
2025-04-28 18:14:51 -0300

    feat: hook new user

--- src/app/interfaces/User.ts
+++ src/app/interfaces/User.ts
@@ -1,9 +1,7 @@
 export interface IUser {
-  created_at: "string";
   email: "string";
   id: "string";
   name: "string";
-  updated_at: "string";
 }
 export interface UsersResponse {
   users: IUser[];
