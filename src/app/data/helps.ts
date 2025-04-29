dd34d7b77923f4668dd90970d085a96b08f9121c
gabriel-lgomes <gabriel_lopes.g@hotmail.com>
2025-04-29 14:59:59 -0300

    feat: help texts

---
+++ src/app/data/helps.ts
@@ -0,0 +1,20 @@
+export const helps = [
+  {
+    image: "/help.png",
+    title: "Precisa de ajuda?",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incidente ut labore et dolore magna aliqua.",
+  },
+  {
+    image: "/heart.png",
+    title: "Por que se registrar?",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incidente ut labore et dolore magna aliqua.",
+  },
+  {
+    image: "/smile.png",
+    title: "O que está acontecendo?...",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incidente ut labore et dolore magna aliqua.",
+  },
+];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,11 @@
+import Image from "next/image";
 import Form from "./components/Form/Form";
 import Grid from "./components/Grid/Grid";
 import Table from "./components/Table/Table";
+import { helps } from "./data/helps";
+import { skills } from "./data/skills";
 
 export default function Home() {
-  const skills = [
-    {
-      image: "/dribbble-square.png",
-      title: "Tipo de Quadra",
-      description: "Society",
-    },
-    {
-      image: "/align-left.png",
-      title: "Nível",
-      description: "Semi-Profissional",
-    },
-    {
-      image: "/trophy.png",
-      title: "Vitórias",
-      description: "345",
-    },
-  ];
-
   return (
     <main className="mt-44">
       <section className="bg-primary py-5">
@@ -45,6 +30,32 @@ export default function Home() {
       <section className="py-5">
         <div className="container max-w-5xl mx-auto px-4">
           <Form />
+        </div>
+      </section>
+      <section className="py-5">
+        <div className="container max-w-5xl mx-auto px-4">
+          <div className="grid lg:grid-cols-3 md:grid-cols-3 grid-cols-1 gap-8">
+            {helps.map((item, index) => (
+              <div key={index}>
+                <p className="text-primary font-medium mb-6">{item.title}</p>
+                <div className="flex items-center gap-3">
+                  <div className="w-1/6">
+                    <Image
+                      src={item.image}
+                      alt={item.title}
+                      width={43}
+                      height={43}
+                    />
+                  </div>
+                  <div className="w-5/6">
+                    <p className="font-medium text-[#707070]">
+                      {item.description}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </section>
     </main>
