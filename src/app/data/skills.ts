514cf1f7c95e07d8c0e90f16c54519ae15b02271
gabriel-lgomes <gabriel_lopes.g@hotmail.com>
2025-04-29 15:00:22 -0300

    feat: skills data

---
+++ src/app/data/skills.ts
@@ -0,0 +1,17 @@
+export const skills = [
+  {
+    image: "/dribbble-square.png",
+    title: "Tipo de Quadra",
+    description: "Society",
+  },
+  {
+    image: "/align-left.png",
+    title: "Nível",
+    description: "Semi-Profissional",
+  },
+  {
+    image: "/trophy.png",
+    title: "Vitórias",
+    description: "345",
+  },
+];
